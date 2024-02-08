3c6b2cc6eacb9afe4bc027aab26298ebd7449765
Ashutosh Chauhan <100701424+theashu02@users.noreply.github.com>
2024-02-09 02:29:26 +0530

    INT-char.cpp

--- INT-char.cpp
+++ INT-char.cpp
@@ -1,8 +1,15 @@
 ## convert int into char
+    // method 1:
+   // string t = to_string(i);
+   // char const *n1 = t.c_str();
+   // s.push_back(*n1);
 
-   string t = to_string(i);
-   char const *n1 = t.c_str();
-   s.push_back(*n1);
+
+   // mehtod 2:
+   // temp.append(to_string(val));
+   // s.push_back(temp);
+   // to_string(val): This function takes an integer value val as input and returns a string representation of that value.
+   // temp.append(...): This method is used to append a string to the end of another string. In this case, it appends the string representation of the integer value val to the string temp.
 
    (int) -> (char)
 
