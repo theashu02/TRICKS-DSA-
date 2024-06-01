a4a9bb555c561f7fdb3f8141c7868a75bc7f7d63
Ashutosh Chauhan <ashutoshchauhan1919@gmail.com>
2024-06-01 15:30:28 +0000

    static cast

---
+++ staticcast.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s = "ashu";
+    int a = static_cast<int> (s[0]);
+    int b = static_cast<int> (s[1]);
+    int c = abs(b - a);
+    cout<<c<<endl;
+    
+    return 0;
+}
