4794c470607e6fe28b3eb8152c580c901f42a4c6
Ashutosh Chauhan <ashutoshchauhan1919@gmail.com>
2024-06-05 22:34:04 +0000

    template for CP

---
+++ Template/template.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3", "unroll-loops")
+#define ll long long
+#define lln long long int
+#define mpp map<int, int>
+#define umpp unordered_map<int, int>
+const int mod = 1e9 + 7;
+
+using namespace std;
+
+auto init = []()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+class Solution
+{
+public:
+    void solve()
+    {
+        // write your code
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Solution obj;
+        obj.solve();
+    }
+
+    return 0;
+}
