8d160d7952743de9050740533c3db6f6604ec7b7
Ashutosh Chauhan <ashutoshchauhan1919@gmail.com>
2024-06-13 13:36:00 +0000

    update template

--- Template/template.cpp
+++ Template/template.cpp
@@ -1,10 +1,19 @@
 #include <bits/stdc++.h>
 #pragma GCC optimize("O3", "unroll-loops")
 #define ll long long
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(), v.end()
 #define lln long long int
 #define mpp map<int, int>
 #define umpp unordered_map<int, int>
 const int mod = 1e9 + 7;
+// Define debugging macros (if necessary)
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << "\n";
+#else
+#define debug(x)
+#endif
 
 using namespace std;
 
@@ -29,6 +38,7 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
 
     int t;
     cin >> t;
