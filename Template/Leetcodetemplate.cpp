59d9241b5fcb03abb6015229e69161dbf05db30f
Ashutosh Chauhan <ashutoshchauhan1919@gmail.com>
2024-06-14 15:44:11 +0000

    leetcode template

---
+++ Template/Leetcodetemplate.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3", "unroll-loops")
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(), v.end()
+#define lln long long int
+#define mpp map<int, int>
+#define umpp unordered_map<int, int>
+const int mod = 1e9 + 7;
+// Define debugging macros (if necessary)
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << "\n";
+#else
+#define debug(x)
+#endif
+
+using namespace std;
+
+auto init = []()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+class Solution{
+public:
+    // function given 
+};
